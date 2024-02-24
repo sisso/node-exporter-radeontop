155985c5cfb2e5b78a77259be30517dec58caaf9
sisso <349345+sisso@users.noreply.github.com>
2024-02-24 17:59:00 -0300

    parsing radeontop data

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use std::error::Error;
+use std::io::BufRead;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // execute a process "radeontop -d -" and proces each output line
+    let mut cmd = std::process::Command::new("radeontop");
+    cmd.arg("-d").arg("-");
+    let mut child = cmd.stdout(std::process::Stdio::piped()).spawn()?;
+    let stdout = child.stdout.as_mut().unwrap();
+    let reader = std::io::BufReader::new(stdout);
+    for line in reader.lines() {
+        let data = parse_radeontop_line(&line?);
+        println!("{:?}", data);
+    }
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub struct RadeonData {
+    timestamp: f64,
+    bus: f64,
+    gpu: f64,
+    ee: f64,
+    vgt: f64,
+    ta: f64,
+    tc: f64,
+    sx: f64,
+    sh: f64,
+    spi: f64,
+    smx: f64,
+    cr: f64,
+    sc: f64,
+    pa: f64,
+    db: f64,
+    cb: f64,
+    vram: f64,
+    gtt: f64,
+    mclk: f64,
+    sclk: f64,
+}
+
+fn parse_radeontop_line(line: &str) -> Result<RadeonData, Box<dyn Error>> {
+    // use regular expression to parse the sample line "1708806303.77193: bus 03, gpu 3.33%, ee 0.00%, vgt 0.83%, ta 0.00%, tc 0.00%, sx 0.83%, sh 0.83%, spi 0.83%, smx 0.83%, cr 4.17%, sc 0.83%, pa 0.00%, db 0.83%, cb 0.83%, vram 6.08% 1489.07mb, gtt 0.39% 126.32mb, mclk 7.69% 0.096ghz, sclk 4.12% 0.098ghz" into RadeonData struct
+    println!("parsing line {:?}", line);
+    let re = regex::Regex::new(
+        r"(\d+).(\d+): bus (\d+), gpu ([\d.]+)%, ee ([\d.]+)%, vgt ([\d.]+)%, ta ([\d.]+)%, tc ([\d.]+)%, sx ([\d.]+)%, sh ([\d.]+)%, spi ([\d.]+)%, smx ([\d.]+)%, cr ([\d.]+)%, sc ([\d.]+)%, pa ([\d.]+)%, db ([\d.]+)%, cb ([\d.]+)%, vram ([\d.]+)% ([\d.]+)mb, gtt ([\d.]+)% ([\d.]+)mb, mclk ([\d.]+)% ([\d.]+)ghz, sclk ([\d.]+)% ([\d.]+)ghz",
+    )?;
+    let Some(caps) = re.captures(line) else {
+        return Err(format!("fail to parse line {:?}", line).into());
+    };
+
+    Ok(RadeonData {
+        timestamp: caps[1].parse::<f64>()?,
+        bus: caps[3].parse()?,
+        gpu: caps[4].parse()?,
+        ee: caps[5].parse()?,
+        vgt: caps[6].parse()?,
+        ta: caps[7].parse()?,
+        tc: caps[8].parse()?,
+        sx: caps[9].parse()?,
+        sh: caps[10].parse()?,
+        spi: caps[11].parse()?,
+        smx: caps[12].parse()?,
+        cr: caps[13].parse()?,
+        sc: caps[14].parse()?,
+        pa: caps[15].parse()?,
+        db: caps[16].parse()?,
+        cb: caps[17].parse()?,
+        vram: caps[18].parse()?, // memory 19
+        gtt: caps[20].parse()?,  // memory 21
+        mclk: caps[22].parse()?,
+        sclk: caps[24].parse()?,
+    })
+}
+
+// create test module
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // create test for parse_radeontop_line
+    #[test]
+    fn test_parse_radeontop_line() {
+        let line = "1708806303.77193: bus 03, gpu 3.33%, ee 0.00%, vgt 0.83%, ta 0.00%, \
+            tc 0.00%, sx 0.83%, sh 0.83%, spi 0.83%, smx 0.83%, cr 4.17%, sc 0.83%, pa 0.00%, db \
+            0.83%, cb 0.83%, vram 6.08% 1489.07mb, gtt 0.39% 126.32mb, mclk 7.69% 0.096ghz, \
+            sclk 4.12% 0.098ghz";
+
+        let data = parse_radeontop_line(line).unwrap();
+        assert_eq!(data.timestamp, 1708806303.0);
+        assert_eq!(data.bus, 3.0);
+        assert_eq!(data.gpu, 3.33);
+        assert_eq!(data.ee, 0.0);
+        assert_eq!(data.vgt, 0.83);
+        assert_eq!(data.ta, 0.0);
+        assert_eq!(data.tc, 0.0);
+        assert_eq!(data.sx, 0.83);
+        assert_eq!(data.sh, 0.83);
+        assert_eq!(data.spi, 0.83);
+        assert_eq!(data.smx, 0.83);
+        assert_eq!(data.cr, 4.17);
+        assert_eq!(data.sc, 0.83);
+        assert_eq!(data.pa, 0.0);
+        assert_eq!(data.db, 0.83);
+        assert_eq!(data.cb, 0.83);
+        assert_eq!(data.vram, 6.08);
+        assert_eq!(data.gtt, 0.39);
+        assert_eq!(data.mclk, 7.69);
+        assert_eq!(data.sclk, 4.12);
+    }
+}
